a6f268226a5f1acc3fca324ea41e665cc8db6a91
Willie  Chapman <williechapman@Willies-MacBook-Pro.local>
2017-05-03 21:03:15 -0700

    Completed index.html adding animations, centering elements, rewrote syntax for header section background image. Also started about.html page. Will add media queries to all pages at the end.

--- js/animations.js
+++ js/animations.js
@@ -35,22 +35,54 @@
 
 
   /* ----- ANIMATIONS ON SCROLL -----*/
+// Header Section H1 and Button Animation
+var $head = $('.head');
+
+$head.waypoint (function (){
+  $head.addClass('js-head-animate fadeInDown')
+}, { offset:'100%' });
+
+var $leftButton = $('.leftButton');
+
+$leftButton.waypoint (function (){
+  $leftButton.addClass('js-leftButton-animate fadeInRight')
+}, { offset:'100%' });
+
+var $rightButton = $('.rightButton');
+
+$rightButton.waypoint (function (){
+  $rightButton.addClass('js-rightButton-animate fadeInLeft')
+}, { offset:'100%' });
+
+// Brief Descript Section SubInfo Animation
 var $subInfoA = $('.subInfoA');
 
 $subInfoA.waypoint (function (){
-  $subInfoA.addClass('js-subInfo-animate-a fadeInLeft');
+  $subInfoA.addClass('js-subInfo-animate-a fadeInDown');
 }, { offset: '85%' });
 
 var $subInfoB = $('.subInfoB');
 
 $subInfoB.waypoint (function (){
-  $subInfoB.addClass('js-subInfo-animate-b fadeInLeft');
-}, { offset: '85%' });
+  $subInfoB.addClass('js-subInfo-animate-b fadeInDown');
+}, { offset: '80%' });
 
 var $subInfoC = $('.subInfoC');
 
 $subInfoC.waypoint (function (){
-  $subInfoC.addClass('js-subInfo-animate-c fadeInLeft');
-}, { offset: '85%' });
+  $subInfoC.addClass('js-subInfo-animate-c fadeInDown');
+}, { offset: '75%' });
+
+// Contact Section Image and Button Animation
+var $mailImage = $('.mailImage');
+
+$mailImage.waypoint (function (){
+  $mailImage.addClass('js-mailImage-animate fadeInLeft'); 
+}, { offset: '75%'});
 
 
+var $contactBtn = $('.contactBtn')
+
+$contactBtn.waypoint (function (){
+  $contactBtn.addClass('js-contactBtn-animate fadeInRight')
+}, { offset: '70%'});
