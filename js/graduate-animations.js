9e1e0a11ea84660b53d2929499e282fa29cbf9cb
Willie  Chapman <williechapman@Willies-MacBook-Pro.local>
2017-05-05 22:38:18 -0700

    Completed the html, css, and animation for graduate.html. Next I must connect all of the links and optimize for mobile use.

---
+++ js/graduate-animations.js
@@ -0,0 +1,48 @@
+// For Rows A-H
+var $rowA = $('.rowA');
+
+$rowA.waypoint( function (){
+	$rowA.addClass('js-rowA-animate fadeIn');
+}, { offset: '95%'});
+
+var $rowB = $('.rowB');
+
+$rowB.waypoint( function (){
+	$rowB.addClass('js-rowB-animate fadeIn');
+}, { offset: '90%'});
+
+var $rowC = $('.rowC');
+
+$rowC.waypoint( function (){
+	$rowC.addClass('js-rowC-animate fadeIn');
+}, { offset: '85%'});
+
+var $rowD = $('.rowD');
+
+$rowD.waypoint( function (){
+	$rowD.addClass('js-rowD-animate fadeIn');
+}, { offset: '80%'});
+
+var $rowE = $('.rowE');
+
+$rowE.waypoint( function (){
+	$rowE.addClass('js-rowE-animate fadeIn');
+}, { offset: '75%'});
+
+var $rowF = $('.rowF');
+
+$rowF.waypoint( function (){
+	$rowF.addClass('js-rowF-animate fadeIn');
+}, { offset: '70%'});
+
+var $rowG = $('.rowG');
+
+$rowG.waypoint( function (){
+	$rowG.addClass('js-rowG-animate fadeIn');
+}, { offset: '65%'});
+
+var $rowH = $('.rowH');
+
+$rowH.waypoint( function (){
+	$rowH.addClass('js-rowH-animate fadeIn');
+}, { offset: '60%'});
